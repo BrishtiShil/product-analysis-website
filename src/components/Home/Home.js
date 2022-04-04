ac53d75a6540608c2f936facde1c4dc3495a3238
Brishti Shil <brishtishil62@gmail.com>
2022-04-04 21:27:55 +0600

    make home page

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,15 +2,19 @@ import React from 'react';
 
 const Home = () => {
     return (
-        <div>
-            <div>
-                <h1>Choose your watch</h1>
-                <p>WatchTime has one of the best watch test selection worldwide.</p>
+        <section>
+            <div style={{ display: "flex" }}>
+                <div>
+                    <h1>Choose your watch</h1>
+                    <h1 style={{ color: "blue" }}>Set your time</h1>
+                    <p style={{ sixe: "bold" }}>WatchTime has one of the best watch test selection worldwide.A watch is something personal. Everyone has their own taste in design and style. Therefore, we do not want to say what a good or bad watch is, but what the signs of a quality watch are in general. And how these signs are easily identifiable.</p>
+                </div>
+                <div>
+                    <img src="/logo.png" alt="" />
+                </div>
             </div>
-            <div>
-                <img src="/logo.png" alt="" />
-            </div>
-        </div>
+            <h1>Customer Reviews</h1>
+        </section>
     );
 };
 
